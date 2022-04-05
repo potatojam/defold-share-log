98f9973cbcadda4872fd686773cf7b8bb35566f2
Bagabum <Bagabum1@gmail.com>
2022-04-05 13:33:47 +0300

    Added listner for logs

--- sharelog/src/main.cpp
+++ sharelog/src/main.cpp
@@ -1,26 +1,92 @@
 
-#define LIB_NAME "HtmlLog"
+#define LIB_NAME "ShareLog"
+#define MODULE_PRIVATE_NAME "sharelog_private"
 
 #include <dmsdk/sdk.h>
 
-static dmExtension::Result AppInitializeSentinel(dmExtension::AppParams* params)
+#ifdef DM_PLATFORM_HTML5
+
+static dmExtension::Result AppInitializeShareLog(dmExtension::AppParams *params)
 {
     return dmExtension::RESULT_OK;
 }
 
-static dmExtension::Result InitializeSentinel(dmExtension::Params* params)
+static dmExtension::Result InitializeShareLog(dmExtension::Params *params)
 {
     return dmExtension::RESULT_OK;
 }
 
-static dmExtension::Result AppFinalizeSentinel(dmExtension::AppParams* params)
+static dmExtension::Result AppFinalizeShareLog(dmExtension::AppParams *params)
 {
     return dmExtension::RESULT_OK;
 }
 
-static dmExtension::Result FinalizeSentinel(dmExtension::Params* params)
+static dmExtension::Result FinalizeShareLog(dmExtension::Params *params)
 {
     return dmExtension::RESULT_OK;
 }
 
-DM_DECLARE_EXTENSION(HtmlLog, LIB_NAME, AppInitializeSentinel, AppFinalizeSentinel, InitializeSentinel, NULL, NULL, FinalizeSentinel)
+#else
+
+#include <string>
+#include <dmsdk/dlib/log.h>
+
+static std::string stringLog;
+
+static void LogListener(dmLog::Severity severity, const char *type, const char *message)
+{
+    //dmLog::LOG_SEVERITY_USER_DEBUG
+    stringLog = stringLog + message;
+}
+
+static int GetShareLogString(lua_State *L)
+{
+    char *cstr = strcpy(new char[stringLog.length() + 1], stringLog.c_str());
+    lua_pushstring(L, cstr);
+    return 1;
+}
+
+// Functions exposed to Lua
+static const luaL_reg Module_methods[] =
+    {
+        {"get_log_string", GetShareLogString},
+        {0, 0}};
+
+static void LuaInit(lua_State *L)
+{
+    int top = lua_gettop(L);
+
+    // Register lua names
+    luaL_register(L, MODULE_PRIVATE_NAME, Module_methods);
+
+    lua_pop(L, 1);
+    assert(top == lua_gettop(L));
+}
+
+static dmExtension::Result AppInitializeShareLog(dmExtension::AppParams *params)
+{
+    stringLog = "";
+    dmLog::RegisterLogListener(&LogListener);
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result InitializeShareLog(dmExtension::Params *params)
+{
+    LuaInit(params->m_L);
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result AppFinalizeShareLog(dmExtension::AppParams *params)
+{
+    dmLog::UnregisterLogListener(&LogListener);
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result FinalizeShareLog(dmExtension::Params *params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+#endif
+
+DM_DECLARE_EXTENSION(ShareLog, LIB_NAME, AppInitializeShareLog, AppFinalizeShareLog, InitializeShareLog, NULL, NULL, FinalizeShareLog)
